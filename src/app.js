909abfd49e42398fdee3fc72494522ba1ebdfe88
Marlon <marlon@ianakiara.local>
2016-06-22 15:52:19 -0300

    new JWT

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import cors       from 'cors';
 import morgan     from 'morgan';
 import express    from 'express';
+import jwt        from 'express-jwt';
 import bodyParser from 'body-parser';
 
 // Routes
-import webservice from './services/webservice/sample';
+import webservice from './v1/services/webservice/sample';
 
 const port = process.env.PORT || 8090;
 const app  = express();
@@ -12,10 +13,17 @@ const app  = express();
 // Express Configurations
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(morgan('combined'))
 app.use(cors());
+app.use(morgan('combined'))
+
+// Validation of API token (API Security)
+app.use(jwt({secret: 'secret'}));
+app.use((err, req, res, next) => {
+  if(err.name === 'UnauthorizedError')
+    res.status(401).send({payload: {err: err.name, stack: 'Invalid Token'}});
+})
 
 // Routes Import
-app.use('/api/webservice', webservice);
+app.use('/v1/webservice', webservice);
 
 app.listen(port, () => console.log('[x] Magic happens on port: ' + port));
